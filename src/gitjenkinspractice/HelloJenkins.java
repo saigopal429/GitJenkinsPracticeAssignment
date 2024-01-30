54f2d77994e596aeff9fed713f72c5fb5069de61
saigopal429 <saigopal429@gmail.com>
2024-01-30 14:25:56 +0530

    jenkinsprogram
    jenkinsprogram

--- src/gitjenkinspractice/HelloJenkins.java
+++ src/gitjenkinspractice/HelloJenkins.java
@@ -1,9 +1,9 @@
 package gitjenkinspractice;
 
-public class HelloWorld {
+public class HelloJenkins {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World....!");
+		System.out.println("Hello Jenkins...!");
 
 	}
 
